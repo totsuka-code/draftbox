7ee03b0bf537ff31b812a0fe7dffac114f2deebf
Hiromu Totsuka <hiromu20020405@gmail.com>
2026-07-17 20:58:35 +0900

    chore: fix lint config and dependency audit

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  ...compat.extends("next"),
+  { ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"] },
+];
+
+export default eslintConfig;
